9ec916de2e257784c4f574dc54508850274b55da
Tobias Leibner <tobias.leibner@googlemail.com>
2015-01-13 09:55:24 +0100

    [hyperbolic.problems] add default problem, fix flux in burgers

--- dune/hdd/hyperbolic/problems/default.hh
+++ dune/hdd/hyperbolic/problems/default.hh
@@ -3,8 +3,8 @@
 // Copyright holders: Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-#ifndef DUNE_HDD_HYPERBOLIC_PROBLEMS_BURGERS_HH
-#define DUNE_HDD_HYPERBOLIC_PROBLEMS_BURGERS_HH
+#ifndef DUNE_HDD_HYPERBOLIC_PROBLEMS_DEFAULT_HH
+#define DUNE_HDD_HYPERBOLIC_PROBLEMS_DEFAULT_HH
 
 #include <memory>
 
@@ -26,7 +26,6 @@
 #include <dune/pymor/functions/checkerboard.hh>
 
 #include "interfaces.hh"
-#include "default.hh"
 
 namespace Dune {
 namespace HDD {
@@ -35,25 +34,27 @@ namespace Problems {
 
 
 template< class E, class D, int d, class R, int r = 1 >
-class Burgers
+class Default
 {
-  Burgers() { static_assert(AlwaysFalse< E >::value, "Not available for these dimensions!"); }
+  Default() { static_assert(AlwaysFalse< E >::value, "Not available for these dimensions!"); }
 };
 
 
 template< class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp >
-class Burgers< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1 >
+class Default< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1 >
   : public ProblemInterface< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1 >
 {
   typedef ProblemInterface< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1 > BaseType;
-  typedef Burgers< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1 > ThisType;
-  typedef Default< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1 > DefaultProblemType;
+  typedef Default< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1 > ThisType;
 
 public:
   typedef typename Dune::Stuff::Functions::Expression
                 < EntityImp, RangeFieldImp, 1, RangeFieldImp, domainDim > DefaultFluxType;
-  typedef typename DefaultProblemType::DefaultFunctionType   DefaultFunctionType;
-  typedef typename DefaultProblemType::DefaultSourceType     DefaultSourceType;
+  typedef typename Dune::Stuff::Functions::Expression
+                < EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1, 1 > DefaultFunctionType;
+  typedef typename Dune::Stuff::Functions::Expression
+                < EntityImp, DomainFieldImp, 1, RangeFieldImp, 1, 1 > DefaultSourceType;
+  typedef typename Dune::Stuff::Functions::Indicator< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1, 1 > IndicatorFunctionType;
 
   typedef typename BaseType::FluxType          FluxType;
   typedef typename BaseType::SourceType        SourceType;
@@ -61,14 +62,22 @@ public:
   typedef typename BaseType::ConfigType        ConfigType;
   typedef typename BaseType::RangeFieldType    RangeFieldType;
 
+  typedef typename IndicatorFunctionType::DomainType    DomainType;
+  typedef typename IndicatorFunctionType::RangeType     RangeType;
+
   static std::string static_id()
   {
     return BaseType::static_id() + ".burgers";
   }
 
+//  virtual std::string type() const override
+//  {
+//    return BaseType::type() + ".burgers";
+//  }
+
   std::string type() const
   {
-    return BaseType::type() + ".burgers";
+    return BaseType::type() + ".default";
   }
 
 private:
@@ -92,13 +101,33 @@ private:
 public:
   static ConfigType default_config(const std::string sub_name = "")
   {
-    ConfigType config = DefaultProblemType::default_config();
+    ConfigType config;
+    config.add(default_grid_config(), "grid");
+    config.add(default_boundary_info_config(), "boundary_info");
     ConfigType flux_config = DefaultFluxType::default_config();
     flux_config["type"] = FluxType::static_id();
     flux_config["variable"] = "u";
     flux_config["expression"] = "1.0/2.0*u[0]*u[0]";
     flux_config["order"] = "2";
     config.add(flux_config, "flux");
+    ConfigType source_config = DefaultSourceType::default_config();
+    source_config["type"] = SourceType::static_id();
+    source_config["variable"] = "u";
+    source_config["expression"] = "0";
+    source_config["order"] = "0";
+    config.add(source_config, "source");
+    ConfigType initial_value_config = DefaultFunctionType::default_config();
+    initial_value_config["type"] = DefaultFunctionType::static_id();
+    initial_value_config["variable"] = "x";
+    initial_value_config["expression"] = "sin(pi*x[0])";
+    initial_value_config["order"] = "10";
+    config.add(initial_value_config, "initial_values");
+    ConfigType boundary_value_config = DefaultFunctionType::default_config();
+    boundary_value_config["type"] = DefaultFunctionType::static_id();
+    boundary_value_config["variable"] = "x";
+    boundary_value_config["expression"] = "x[0]";
+    boundary_value_config["order"] = "1";
+    config.add(boundary_value_config, "boundary_values");
     if (sub_name.empty())
       return config;
     else {
@@ -112,21 +141,23 @@ public:
                                             const std::string sub_name = static_id())
   {
     const ConfigType config = cfg.has_sub(sub_name) ? cfg.sub(sub_name) : cfg;
+    const std::shared_ptr< const DefaultFluxType > flux(DefaultFluxType::create(config.sub("flux")));
     const std::shared_ptr< const DefaultSourceType > source(DefaultSourceType::create(config.sub("source")));
     const std::shared_ptr< const DefaultFunctionType > initial_values(DefaultFunctionType::create(config.sub("initial_values")));
     const ConfigType grid_config = config.sub("grid");
     const ConfigType boundary_info = config.sub("boundary_info");
     const std::shared_ptr< const DefaultFunctionType > boundary_values(DefaultFunctionType::create(config.sub("boundary_values")));
-    return Stuff::Common::make_unique< ThisType >(source, initial_values,
+    return Stuff::Common::make_unique< ThisType >(flux, source, initial_values,
                                                   grid_config, boundary_info, boundary_values);
   } // ... create(...)
 
-  Burgers(const std::shared_ptr< const SourceType > source = std::make_shared< DefaultSourceType >("u", "0", 0),
+  Default(const std::shared_ptr< const FluxType > flux = std::make_shared< DefaultFluxType >("u", "1.0/2.0*u[0]*u[0]", 2),
+          const std::shared_ptr< const SourceType > source = std::make_shared< DefaultSourceType >("u", "0", 0),
           const std::shared_ptr< const FunctionType > initial_values = std::make_shared< DefaultFunctionType >("x", "sin(pi*x[0])", 10),
           const ConfigType& grid_config = default_grid_config(),
           const ConfigType& boundary_info = default_boundary_info_config(),
           const std::shared_ptr< const FunctionType > boundary_values = std::make_shared< DefaultFunctionType >("x", "0", 0))
-    : flux_(std::make_shared< DefaultFluxType >("u", "1.0/2.0*u[0]*u[0]", 2))
+    : flux_(flux)
     , source_(source)
     , initial_values_(initial_values)
     , grid_config_(grid_config)
@@ -178,4 +209,4 @@ private:
 } // namespace HDD
 } // namespace Dune
 
-#endif // DUNE_HDD_HYPERBOLIC_PROBLEMS_BURGERS_HH
+#endif // DUNE_HDD_HYPERBOLIC_PROBLEMS_DEFAULT_HH
